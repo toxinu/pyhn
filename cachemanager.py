46604481081cd85f80bebb3cfaf1d3eb669fc80e
toxinu <toxinu@gmail.com>
2013-01-05 18:11:11 +0100

    Add cache manager

---
+++ cachemanager.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import pickle
+import datetime
+
+from hnapi import HackerNewsAPI
+
+cache_path = os.path.join(os.environ.get('HOME', './'), '.hn.cache')
+
+class cacheManager(object):
+	def __init__(self, path=cache_path):
+		self.path = path
+		self.api = HackerNewsAPI()
+
+		if not os.path.exists(self.path):
+			self.refresh_cache()
+
+	def is_outdated(self, which="top"):
+		if not os.path.exists(self.path):
+			return True
+
+		cache = pickle.load(open(self.path))
+		if not cache.get(which, False):
+			return True
+
+		cache_age = datetime.datetime.today() - cache[which]['date']
+		if cache_age.seconds > 5*60:
+			return True
+		else:
+			return False
+
+	def refresh(self, which="top"):
+		if which == "top":
+			stories = self.api.getTopStories()
+		elif which == "newest":
+			stories = self.api.getNewestStories()
+		elif which == "best":
+			stories = self.api.getBestStories()
+		else:
+			raise Exception('Bad choice, can only refresh: top, newest and best stories')
+
+		cache = {}
+		if os.path.exists(self.path):
+			cache = pickle.load(open(self.path))
+
+		cache[which] = {'stories':stories, 'date':datetime.datetime.today()}
+		pickle.dump(cache, open(self.path, 'w'))
+
+	def get_stories(self, which="top"):
+		cache = []
+		if os.path.exists(self.path):
+			cache = pickle.load(open(self.path))
+	
+		if not cache.get(which, False):
+			return []
+		else:
+			return cache[which]['stories']
--- gethn.py
+++ gethn.py
@@ -1,29 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
-import pickle
-import datetime
-
 import gui
+from cachemanager import cacheManager
 
-cache_path = os.path.join(os.environ.get('HOME', './'), '.hn.cache')
-
-def refreshCache():
-    from hnapi import HackerNewsAPI
-    print('==> Refreshing HN cache')
-    hn = HackerNewsAPI()
-    stories = hn.getTopStories()
-    cache = {'date':datetime.datetime.today(),'stories':stories}
-    pickle.dump(cache, open(cache_path, 'w'))
-    return stories
-
-if os.path.exists(cache_path):
-    cache = pickle.load(open(cache_path))
-    stories = cache['stories']
-    cacheAge = datetime.datetime.today() - cache['date']
-    if cacheAge.seconds > 5*60:
-        stories = refreshCache()
-else:
-    stories = refreshCache()
-
-gui.render(stories)
+cachemanager = cacheManager()
+hn_gui = gui.HNGui(cachemanager)
+hn_gui.main()
