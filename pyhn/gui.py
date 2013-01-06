9bbe5083679a2f94099db0da8a7d77aec74b9d25
alexzhan <alexzhan12@gmail.com>
2013-01-06 15:31:38 +0800

    change to ctrl + shift + f

--- pyhn/gui.py
+++ pyhn/gui.py
@@ -131,7 +131,7 @@ class HNGui(object):
         elif input is 'j':
             if self.listbox.focus_position + 1 in self.walker.positions():
                 self.listbox.set_focus(self.walker.next_position(self.listbox.focus_position))
-        elif input is 'F':
+        elif input is 'shift ctrl f':
             if self.listbox.focus_position + 14 in self.walker.positions():
                 self.listbox.set_focus(self.listbox.focus_position + 14)
 
