6664227b73579b1b80c2e14a979a1d0ba23daf29
socketubs <geoffrey@lehee.name>
2013-01-06 12:38:46 +0100

    Add configuration file

--- pyhn/cachemanager.py
+++ pyhn/cachemanager.py
@@ -4,22 +4,25 @@ import pickle
 import datetime
 
 from pyhn.hnapi import HackerNewsAPI
-
-cache_path = os.path.join(os.environ.get('HOME', './'), '.pyhn.cache')
+from pyhn.config import Config
 
 class cacheManager(object):
-	def __init__(self, path=cache_path):
-		self.path = path
+	def __init__(self, cache_path=None):
+		self.cache_path = cache_path
+		if cache_path is None:
+			config = Config()
+			self.cache_path = config.parser.get('settings', 'cache')
+
 		self.api = HackerNewsAPI()
 
-		if not os.path.exists(self.path):
+		if not os.path.exists(self.cache_path):
 			self.refresh()
 
 	def is_outdated(self, which="top"):
-		if not os.path.exists(self.path):
+		if not os.path.exists(self.cache_path):
 			return True
 
-		cache = pickle.load(open(self.path))
+		cache = pickle.load(open(self.cache_path))
 		if not cache.get(which, False):
 			return True
 
@@ -40,16 +43,16 @@ class cacheManager(object):
 			raise Exception('Bad choice, can only refresh: top, newest and best stories')
 
 		cache = {}
-		if os.path.exists(self.path):
-			cache = pickle.load(open(self.path))
+		if os.path.exists(self.cache_path):
+			cache = pickle.load(open(self.cache_path))
 
 		cache[which] = {'stories':stories, 'date':datetime.datetime.today()}
-		pickle.dump(cache, open(self.path, 'w'))
+		pickle.dump(cache, open(self.cache_path, 'w'))
 
 	def get_stories(self, which="top"):
 		cache = []
-		if os.path.exists(self.path):
-			cache = pickle.load(open(self.path))
+		if os.path.exists(self.cache_path):
+			cache = pickle.load(open(self.cache_path))
 	
 		if not cache.get(which, False):
 			return []
