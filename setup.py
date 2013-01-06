13fdf5a4d231b306e120de0b5b23ed2f677daf41
socketubs <geoffrey@lehee.name>
2013-01-06 14:16:46 +0100

    Add classifiers

--- setup.py
+++ setup.py
@@ -42,6 +42,13 @@ setup(
 		'Natural Language :: English',
 		'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
 		'Programming Language :: Python',
+		'Programming Language :: Python :: 2',
 		'Programming Language :: Python :: 2.6',
-		'Programming Language :: Python :: 2.7')
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.0',
+		'Programming Language :: Python :: 3.1',
+		'Programming Language :: Python :: 3.2',
+		'Programming Language :: Python :: 3.3'
+		)
 )
