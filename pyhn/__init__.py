8492ea59eb43cde707895da7692715f53315a88e
toxinu <toxinu@gmail.com>
2013-11-18 15:25:52 +0100

    0.2.3

--- pyhn/__init__.py
+++ pyhn/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'pyhn'
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 __author__ = 'Geoffrey Lehée'
 __license__ = 'AGPL3'
 __copyright__ = 'Copyright 2013 Geoffrey Lehée'
