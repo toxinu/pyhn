be14aa554c723be0a5d79deb9a4426317c5f2783
Geoffrey LEHEE <g.lehee@jurismarches.com>
2014-02-24 09:49:56 +0100

    Release 0.2.4

--- pyhn/__init__.py
+++ pyhn/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'pyhn'
-__version__ = '0.2.3'
+__version__ = '0.2.4'
 __author__ = 'Geoffrey Lehée'
 __license__ = 'AGPL3'
-__copyright__ = 'Copyright 2013 Geoffrey Lehée'
+__copyright__ = 'Copyright 2014 Geoffrey Lehée'
