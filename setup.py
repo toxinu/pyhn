b71fe095dd300122901ace396af9fb4492c8dbe2
Geoffrey LEHEE <g.lehee@jurismarches.com>
2015-01-19 10:42:40 +0100

    Python 3.4 classifier

--- setup.py
+++ setup.py
@@ -49,5 +49,6 @@ setup(
         'Programming Language :: Python :: 3.0',
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3']
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4']
 )
