9cd0cee475f265cb2f60e792fdcc412285a0d034
toxinu <toxinu@gmail.com>
2015-06-03 14:15:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,16 @@ import os
 import re
 import sys
 
+from codecs import open
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
 
 packages = [
     'pyhn',
@@ -24,26 +30,25 @@ packages = [
     'pyhn.lib.bs4_py3',
     'pyhn.lib.bs4_py3.builder']
 
+requires = ['urwid==1.3.0']
 
-def get_version():
-    VERSIONFILE = os.path.join('pyhn', '__init__.py')
-    initfile_lines = open(VERSIONFILE, 'rt').readlines()
-    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
-    for line in initfile_lines:
-        mo = re.search(VSRE, line, re.M)
-        if mo:
-            return mo.group(1)
-    raise RuntimeError('Unable to find version string in %s.' % (VERSIONFILE,))
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    sys.exit()
+version = ''
+with open('pyhn/__init__.py', 'r') as fd:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
+
+with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
 
 setup(
     name='pyhn',
-    version=get_version(),
+    version=version,
     description='Hacker News in your terminal',
-    long_description=open('README.rst').read(),
+    long_description=readme,
     license=open("LICENSE").read(),
     author="Geoffrey Lehée",
     author_email="hello@socketubs.org",
@@ -51,12 +56,12 @@ setup(
     keywords="python hackernews hn terminal commandline",
     packages=packages,
     scripts=['scripts/pyhn'],
-    install_requires=['urwid'],
-    package_data={'': ['LICENSE'], 'pyhn.lib.requests': ['*.pem']},
+    install_requires=requires,
+    package_data={'': ['LICENSE', 'NOTICE'], 'pyhn.lib.requests': ['*.pem']},
     package_dir={'pyhn': 'pyhn'},
     include_package_data=True,
     classifiers=[
-        'Intended Audience :: Developers',
+        'Development Status :: 5 - Production/Stable',
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
