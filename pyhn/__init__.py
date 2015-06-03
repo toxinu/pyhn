7e6d44e58d8efde244438ad2d84ff704ebb5ddb2
toxinu <toxinu@gmail.com>
2015-06-03 14:31:10 +0200

    0.3.4

--- pyhn/__init__.py
+++ pyhn/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'pyhn'
-__version__ = '0.3.3'
+__version__ = '0.3.4'
 __author__ = 'Geoffrey Lehée'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015 Geoffrey Lehée'
