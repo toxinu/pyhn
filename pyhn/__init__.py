ee6ef1f8cf7189dc0157a0d9428bd4235b9d9a8f
Geoffrey Lehée <glehee@oasiswork.fr>
2016-08-05 12:25:07 +0200

    0.3.6

--- pyhn/__init__.py
+++ pyhn/__init__.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 __title__ = 'pyhn'
-__version__ = '0.3.5'
+__version__ = '0.3.6'
 __author__ = 'Geoffrey Lehée'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2016 Geoffrey Lehée'
