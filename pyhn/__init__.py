4e3ca891cc98bda0ffdccc0ef0a542091fac9afa
toxinu <toxinu@gmail.com>
2016-08-17 11:48:34 +0200

    Bump version to 0.3.8

--- pyhn/__init__.py
+++ pyhn/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'pyhn'
-__version__ = '0.3.7'
+__version__ = '0.3.8'
 __author__ = 'Geoffrey Lehée'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2016 Geoffrey Lehée'
