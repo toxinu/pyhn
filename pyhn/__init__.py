f2a68c5f0e41ff0bff2ee6397604dec9b8997423
toxinu <toxinu@gmail.com>
2020-12-03 00:05:25 +0900

    Bump version

--- pyhn/__init__.py
+++ pyhn/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'pyhn'
-__version__ = '0.3.9'
+__version__ = '0.3.10'
 __author__ = 'toxinu'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2016 toxinu'
+__copyright__ = 'Copyright 2020 toxinu'
